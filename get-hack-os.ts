cae27c499ecd1dc3394eb4b5fc86c7b6a9926a44
Aleksei Kolesnikov <kotekoder@gmail.com>
2025-11-22 14:16:52 +0200

    Update get-hack-os.ts

--- get-hack-os.ts
+++ get-hack-os.ts
@@ -4,10 +4,12 @@ export async function main(ns: NS) {
     "hack-os/install.ts", 
     "hack-os/core/install.ts",
     "hack-os/core/default-config.json",
-    "hack-os/core/deamons/init.ts",
+    "hack-os/core/bins/init.ts",
     "hack-os/core/modules/hack-os.ts",
     "hack-os/core/modules/logger.ts",
-    "hack-os/core/modules/utils.ts"
+    "hack-os/core/modules/utils.ts",
+    "hack-os/core/scripts/boot.ts",
+    "hack-os/core/scripts/uninstall.ts",
   ]
   ns.tprintf("Getting HACK-OS")
   let promise:Promise<any>|null = null
