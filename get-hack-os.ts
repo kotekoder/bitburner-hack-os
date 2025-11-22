569cbaf7181598ef81d62921d1e22313be50b70e
Aleksei Kolesnikov <kotekoder@gmail.com>
2025-11-22 13:03:04 +0200

    Update get-hack-os.ts

--- get-hack-os.ts
+++ get-hack-os.ts
@@ -4,7 +4,7 @@ export async function main(ns: NS) {
   let files = [
     "hack-os/install.ts", 
     "hack-os/core/install.ts",
-    "hack-os/core/default-config.json"
+    "hack-os/core/default-config.json",
     "hack-os/core/deamons/init.ts",
     "hack-os/core/modules/hack-os.ts",
     "hack-os/core/modules/logger.ts",
