70f528d063719c6e8d50307dd963810c433233cd
Aleksei Kolesnikov <kotekoder@gmail.com>
2025-11-22 13:14:49 +0200

    Update install.ts

--- hack-os/install.ts
+++ hack-os/install.ts
@@ -2,7 +2,7 @@ import {HackOSConfig} from "./core/modules/hack-os"
 
 export async function main(ns: NS) {
   ns.tprintf("Installing HACK-OS")
-  let config = new HackOSConfig() 
+  let config = new HackOSConfig(ns) 
   let installerPath = config.scriptPath
   ns.run(installerPath+"core/install.ts")
   while(ns.ps().length>1){
