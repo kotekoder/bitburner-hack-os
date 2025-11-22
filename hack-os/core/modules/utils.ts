3f786ea7f44cce0ebc611fb6d970b0701ab1e660
Aleksei Kolesnikov <kotekoder@gmail.com>
2025-11-22 14:14:31 +0200

    Delete hack-os/core/modules/utils.ts

--- hack-os/core/modules/utils.ts
+++
@@ -1,7 +0,0 @@
-export function injectCommand(commandLine: string) {
-  const terminalInput = eval('document').getElementById("terminal-input");
-  terminalInput.value = commandLine;
-  const handler = Object.keys(terminalInput)[1];
-  terminalInput[handler].onChange({ target: terminalInput });
-  terminalInput[handler].onKeyDown({ key: 'Enter', preventDefault: () => null });
-}
