c4cedcecf6947a139ad895c7e98022d80045f1cf
Aleksei Kolesnikov <kotekoder@gmail.com>
2025-11-22 12:55:57 +0200

    Update get-hack-os.ts

--- get-hack-os.ts
+++ get-hack-os.ts
@@ -1,6 +1,23 @@
 export async function main(ns: NS) {
-  await Promise.all([
-    ns.wget("url", "target").then(() => ns.tprintf("target")),
-    ns.wget("url", "target").then(() => ns.tprintf("target"))
-  ])
+  let URL_BASE = "https://raw.githubusercontent.com/kotekoder/bitburner-hack-os/refs/heads/main/"
+  let TOKEN = "?token=GHSAT0AAAAAADPC4M4ANLFIH5ITZJAPINIW2JBSNGA" 
+  let files = [
+    "hack-os/install.ts", 
+    "hack-os/core/install.ts",
+    "hack-os/core/default-config.json"
+    "hack-os/core/deamons/init.ts",
+    "hack-os/core/modules/hack-os.ts",
+    "hack-os/core/modules/logger.ts",
+    "hack-os/core/modules/utils.ts"
+  ]
+  ns.tprintf("Getting HACK-OS")
+  let promise:Promise<any>|null = null
+  files.forEach(f=>{
+    if (promise == null)
+      promise = ns.wget(URL_BASE+f+TOKEN,f).then(()=>ns.tprintf(f))
+    else
+      promise = Promise.all([promise,ns.wget(URL_BASE+f+TOKEN,f).then(()=>ns.tprintf(f))])
+  })
+  await promise
+  ns.tprintf("Done")
 }
